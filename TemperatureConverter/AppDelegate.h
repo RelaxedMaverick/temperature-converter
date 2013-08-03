cf5b7d0c92240b41090f41fe5d4bc29c7f884d88
Rohit Dialani <rohitd@yahoo-inc.com>
2013-08-03 19:44:23 +0530

    Vanilla project

---
+++ TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Rohit Dialani on 03/08/13.
+//  Copyright (c) 2013 Rohit Dialani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
